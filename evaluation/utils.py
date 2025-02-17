68bfdfed946518fb7e8cccb720e1526f1b072003
Kehan Li <78654844+lkhl@users.noreply.github.com>
2025-02-17 14:56:42 +0800

    Update utils.py

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -20,105 +20,59 @@ def to_cuda(packed_data):
     return packed_data
 
 
-class BackgroundGenerator(threading.Thread):
-    """
-    the usage is below
-    >> for batch in BackgroundGenerator(my_minibatch_iterator):
-    >>    doit()
-    More details are written in the BackgroundGenerator doc
-    >> help(BackgroundGenerator)
-    """
-    def __init__(self, generator, max_prefetch=10):
-        """
-        This function transforms generator into a background-thead generator.
-        :param generator: generator or genexp or any
-        It can be used with any minibatch generator.
-        It is quite lightweight, but not entirely weightless.
-        Using global variables inside generator is not recommended (may raise GIL and zero-out the
-        benefit of having a background thread.)
-        The ideal use case is when everything it requires is store inside it and everything it
-        outputs is passed through queue.
-        There's no restriction on doing weird stuff, reading/writing files, retrieving
-        URLs [or whatever] wlilst iterating.
-        :param max_prefetch: defines, how many iterations (at most) can background generator keep
-        stored at any moment of time.
-        Whenever there's already max_prefetch batches stored in queue, the background process will halt until
-        one of these batches is dequeued.
-        !Default max_prefetch=1 is okay unless you deal with some weird file IO in your generator!
-        Setting max_prefetch to -1 lets it store as many batches as it can, which will work
-        slightly (if any) faster, but will require storing
-        all batches in memory. If you use infinite generator with max_prefetch=-1, it will exceed the RAM size
-        unless dequeued quickly enough.
-        """
-        super().__init__()
-        self.queue = queue.Queue(max_prefetch)
-        self.generator = generator
-        self.daemon = True
-        self.exit_event = threading.Event()
-        self.start()
-
-    def run(self):
-        for item in self.generator:
-            if self.exit_event.is_set():
-                break
-            self.queue.put(item)
-        self.queue.put(None)
-
-    def next(self):
-        next_item = self.queue.get()
-        if next_item is None:
-            raise StopIteration
-        return next_item
-
-    # Python 3 compatibility
-    def __next__(self):
-        return self.next()
-
-    def __iter__(self):
-        return self
-
-
 class CUDADataLoader(DataLoader):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stream = torch.cuda.Stream() # create a new cuda stream in each process
-
-    def __iter__(self):
-        self.iter = super().__iter__()
-        self.iter = BackgroundGenerator(self.iter)
-        self.preload()
-        return self
-
-    def _shutdown_background_thread(self):
-        if not self.iter.is_alive():
-            # avoid re-entrance or ill-conditioned thread state
-            return
-        # Set exit event to True for background threading stopping
-        self.iter.exit_event.set()
-        # Exhaust all remaining elements, so that the queue becomes empty,
-        # and the thread should quit
-        for _ in self.iter:
-            pass
-        # Waiting for background thread to quit
-        self.iter.join()
+        self.queue = queue.Queue(64)
 
     def preload(self):
-        self.batch = next(self.iter, None)
-        if self.batch is None:
+        batch = next(self.iter)
+        if batch is None:
             return None
+        torch.cuda.current_stream().wait_stream(self.stream)  # wait tensor to put on GPU
         with torch.cuda.stream(self.stream):
-            to_cuda(self.batch)
+            batch = to_cuda(batch)
+        self.queue.put(batch)
+
+    def __iter__(self):
+        # setting a queue for storing prefetched data
+        self.queue.queue.clear()
+        # reset data iterator
+        self.iter = super().__iter__()
+        # starting a new thread to prefetch data
+        def data_to_cuda_then_queue():
+            while True:
+                try:
+                    self.preload()
+                except StopIteration:
+                    break
+            # NOTE: end flag for the queue
+            self.queue.put(None)
+
+        self.thread = threading.Thread(target=data_to_cuda_then_queue, args=())
+        self.thread.daemon = True
+
+        (self.preload() for _ in range(16))
+        self.thread.start()
+        return self
 
     def __next__(self):
-        torch.cuda.current_stream().wait_stream(self.stream)  # wait tensor to put on GPU
-        batch = self.batch
-        if batch is None:
+        next_item = self.queue.get()
+        # NOTE: __iter__ will be stopped when __next__ raises StopIteration 
+        if next_item is None:
             raise StopIteration
-        self.preload()
-        return batch
+        return next_item
 
-    # Signal for shutting down background thread
-    def shutdown(self):
-        # If the dataloader is to be freed, shutdown its BackgroundGenerator
-        self._shutdown_background_thread()
+    def __del__(self):
+        # NOTE: clean up the thread
+        try:
+            self.thread.join(timeout=10)
+        finally:
+            if self.thread.is_alive():
+                self.thread._stop()
+        # NOTE: clean up the stream
+        self.stream.synchronize()
+        # NOTE: clean up the queue
+        self.queue.queue.clear()
