4a5a18489534d2e00caf6c2c9a2529d8a51c907d
peterdou <peterdou@tencent.com>
2025-07-10 23:05:32 +0200

    add a tool script for the dataset preparing.

---
+++ tools/extract_img_par.py
@@ -0,0 +1,39 @@
+import pyarrow.parquet as pq
+import json
+
+src_file = "/Users/peterdou/Downloads/train-00000-of-00005.parquet"
+dst_imgs_dir = "/Users/peterdou/Documents/captioning/LLaVA-OneVision-Data/images"
+dst_json_file = (
+    "/Users/peterdou/Documents/captioning/LLaVA-OneVision-Data/annotations.json"
+)
+
+table = pq.read_table(src_file)
+print("table:", table.column_names)
+
+name_pattern = "image_{}.jpg"
+maxnum: int = 0
+MAXNUM = 2  # any limit?
+for idx, img_bytes in enumerate(table["image"]):
+    maxnum += 1
+    if -1 != MAXNUM and maxnum > MAXNUM:
+        break
+    # save image
+    name = name_pattern.format(idx)
+    with open(f"{dst_imgs_dir}/{name}", "wb") as f:
+        f.write(img_bytes.as_py()["bytes"])
+
+maxnum = 0
+json_dic = []  # the dest json data
+item = {}  # single data item
+for idx, img_bytes in enumerate(table["conversations"]):
+    maxnum += 1
+    if -1 != MAXNUM and maxnum > MAXNUM:
+        break
+    name = name_pattern.format(idx)
+    item["image"] = [f"{dst_imgs_dir}/{name}"]
+    item["conversations"] = img_bytes.as_py()
+    json_dic.append(item.copy())
+
+# save json
+with open(dst_json_file, "w") as d:
+    json.dump(json_dic, d)
